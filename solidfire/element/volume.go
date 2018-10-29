80c372fd1c623a1a3b8215e72551ff7598ed7e37
Adrian Todorov <adrian.ttodorov@gmail.com>
2018-10-29 16:27:15 +0100

    add extra fields to the volume struct

--- solidfire/element/volume.go
+++ solidfire/element/volume.go
@@ -18,9 +18,18 @@ type ListVolumesResult struct {
 }
 
 type Volume struct {
-	Name     string `json:"name"`
-	VolumeID int    `json:"volumeID"`
-	Iqn      string `json:"iqn"`
+	Name               string `json:"name"`
+	VolumeID           int    `json:"volumeID"`
+	Iqn                string `json:"iqn"`
+	Access             string `json:"access"`
+	AccountID          int    `json:"accountID"`
+	BlockSize          int    `json:"blockSize"`
+	Enable512e         bool   `json:"enable512e"`
+	ScsiEUIDeviceID    string `json:"scsiEUIDeviceID"`
+	ScsiNAADeviceID    string `json:"ScsiNAADeviceID"`
+	Status             string `json:"status"`
+	VirtualVolumeID    int    `json:"virtualVolumeID"`
+	VolumeAccessGroups []int  `json:"volumeAccessGroups"`
 }
 
 func (c *Client) GetVolumeByID(id string) (Volume, error) {
