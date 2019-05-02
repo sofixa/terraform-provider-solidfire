d6a3e2e68224bd4e5b3ad055281cb983722c5245
Adrian Todorov <adrian.ttodorov@gmail.com>
2019-05-02 19:09:25 +0200

    fix a missing comma

--- solidfire/resource_solidfire_volume.go
+++ solidfire/resource_solidfire_volume.go
@@ -72,7 +72,7 @@ func resourceSolidFireVolume() *schema.Resource {
 			"access": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default: "readWrite"
+				Default:  "readWrite",
 			},
 			"account_id": {
 				Type:     schema.TypeInt,
